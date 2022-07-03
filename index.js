7080d8a4e8db45fb5b9799c1e72cb69f462a2367
Petr Hurtak <petr.hurtak@gmail.com>
2022-07-03 16:41:22 +0200

    new custom rule

--- index.js
+++ index.js
@@ -8,24 +8,29 @@ module.exports = {
   ],
   plugins: ["simple-import-sort"],
   rules: {
+    // Enable rules from plugins
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
 
     // Disable incompatible rules
-    "unicorn/prefer-module": 0, // Not all projects are module ready yet
-    "unicorn/prevent-abbreviations": 0, // Does not work well with React, revisit https://github.com/sindresorhus/eslint-plugin-unicorn/issues/1639
+    "unicorn/prefer-module": "off", // Not all projects are module ready yet
+    "unicorn/prevent-abbreviations": "off", // Does not work well with React, revisit https://github.com/sindresorhus/eslint-plugin-unicorn/issues/1639
   },
 
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
       extends: [
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "prettier",
       ],
-      parser: "@typescript-eslint/parser",
+      rules: {
+        // Our custom rules
+		"@typescript-eslint/consistent-type-definitions": ["error", "type"]
+      }
     },
   ],
 };
