f0b4141e0d7eeea5903d8501ac0c9a7dafd32a3b
Petr Hurtak <petr.hurtak@gmail.com>
2022-07-03 15:54:55 +0200

    fix error

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-module.exports = require('./.eslintrc.json')
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+    "standard",
+    "prettier",
+  ],
+  plugins: ["simple-import-sort"],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+
+    // Disable incompatible rules
+    "unicorn/prefer-module": 0, // Not all projects are module ready yet
+    "unicorn/prevent-abbreviations": 0, // Does not work well with React, revisit https://github.com/sindresorhus/eslint-plugin-unicorn/issues/1639
+  },
+
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
+      ],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+};
